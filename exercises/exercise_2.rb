6d2e14518790065d8cb5029ea81da9b2df0efc0f
April Mulvey <april.mulvey@gmail.com>
2016-11-09 01:33:26 +0000

    completed activity 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,10 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+class Stores < ActiveRecord::Base
+  @store1 = Store.find(1)
+  @store2 = Store.find(2)
+
+  @store1.name = "Edmonton"
+  @store1.save
+end
